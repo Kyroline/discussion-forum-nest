a218a2fe80b5f85a852eaece6070c93b096f53b4
Kyroline <rizky.w.dewantoro@gmail.com>
2024-08-02 20:08:00 +0700

    Removed community module

--- src/communities/communities.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CommunitiesService } from './communities.service';
-import { CommunitiesController } from './communities.controller';
-
-@Module({
-  controllers: [CommunitiesController],
-  providers: [CommunitiesService],
-})
-export class CommunitiesModule {}
